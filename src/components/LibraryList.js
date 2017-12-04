8a09182a6c768262f7875f4dc9ac00487ac8c4ac
origin <shyamal.punekar@gmail.com>
2017-12-04 15:32:43 -0800

    initial set up

---
+++ src/components/LibraryList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { ListView } from 'react-native';
+import { connect } from 'react-redux';
+import ListItem from './ListItem';
+
+class LibraryList extends Component {
+  componentWillMount(){
+    const ds = new ListView.DataSource({
+      rowHaschanged: (r1, r2) => r1 !== r2
+    });
+    this.DataSource = ds.cloneWithRows(this.this.props..libraries);
+  }
+
+renderRow(library){
+  return <ListItem  library={library}/>
+}
+
+  render() {
+    return(
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps) (LibraryList);
